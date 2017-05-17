c03a23798c5690e96b711e46277937d5508fa7a2
cookerlyk <kylecookerly@gmail.com>
2017-05-17 00:20:14 -0700

    Breaking out the chip8 struct into its own file due to circular import issues

---
+++ src/chip8_t.h
@@ -0,0 +1,31 @@
+/*
+*
+* Definition of the chip8 struct. 
+*
+*/
+
+#define NUM_V_REGISTERS 16
+#define TOTAL_RAM 4096
+#define STACK_SIZE 16
+#define TRUE 1
+#define FALSE 0
+
+
+typedef struct Chip8_t Chip8;
+
+struct Chip8_t {
+    uint8_t ram[TOTAL_RAM];          // 4k of memory
+    uint16_t stack[STACK_SIZE];      // stack, stores up to 16 levels
+
+    // registers 
+    uint8_t V[NUM_V_REGISTERS];      // general purpose registers (0 - 14) and the carry flag register (15)
+    uint16_t I_reg;                  // index register
+    uint16_t pc_reg;                 // pc register
+    uint16_t sp_reg;                 // stack pointer register
+
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+
+    uint16_t current_op;             // current opcode being executed by the system
+
+};
